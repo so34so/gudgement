8ce9321178b603b500da6560a82258dff1a376ff
yms1789 <yms1789@naver.com>
2023-09-05 17:08:13 +0900

    📦️Chore: 패키지 수정
    - react navigation을 위한 코드 추가
    - reactotron-react-query를 위한 코드 추가 및 라이브러리 설치

--- DontSpend/App.tsx
+++ DontSpend/App.tsx
@@ -1,13 +1,16 @@
 /// <reference types="nativewind/types" />
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "react-query";
 import "react-native-gesture-handler";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { queryClient } from "./queryClient";
+
+if (__DEV__) {
+  import("./reactotron");
+}
 
 
-
-const queryClient = new QueryClient();
 
 function App(): JSX.Element {
 
@@ -15,7 +18,6 @@ function App(): JSX.Element {
     <QueryClientProvider client={queryClient}>
       <GestureHandlerRootView style={{ flex: 1 }}>
         <NavigationContainer>
-
         </NavigationContainer>
       </GestureHandlerRootView>
     </QueryClientProvider>

a64d25acece7b1af98aa2e0aab72b132ac21f6df
yms1789 <yms1789@naver.com>
2023-09-05 14:17:56 +0900

    📰New: Reactotron 적용 파일 추가

--- DontSpend/index.js
+++ DontSpend/index.js
@@ -1,9 +1,14 @@
-/**
- * @format
- */
+if (__DEV__) {
+  void import("./ReactotronConfig").then(() =>
+    console.log("Reactotron Configured"),
+  );
+  /**
+   * @format
+   */
+}
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
 
 AppRegistry.registerComponent(appName, () => App);

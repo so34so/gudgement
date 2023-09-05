a52f524f1eb1707d1aa7b07e569366cd9ace61ab
KwonMinWoo <kwmw0427@naver.com>
2023-09-05 21:34:19 +0900

    🚀Feat: Backend 환경 구축

---
+++ BE/Gudgenet/src/main/java/com/example/gudgement/GudgementApplication.java
@@ -0,0 +1,11 @@
+package com.example.gudgement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GudgementApplication {
+
+    public static void main(String[] args) {SpringApplication.run(GudgementApplication.class, args);
+    }
+}
---
+++ BE/Gudgenet/src/test/java/com/example/gudgement/GudgementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gudgement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GudgementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

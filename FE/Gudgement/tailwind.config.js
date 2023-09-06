6facc61f6b71d862638b7077c6e170bfafdbb1c3
kanghaeven <meerhae@gmail.com>
2023-09-06 09:46:57 +0900

    💄Design: colors 변수 추가
    - 섹상명10~90 : 투명도 조절 variation
    - 색상명01~09 : 명도 조절 variation
    - 피그마 변수와 동일하게 가져감

--- FE/Gudgement/tailwind.config.js
+++ FE/Gudgement/tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}", "./App.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#000000',
+        black50: '#00000080',
+        black70: '#000000b2',
+        buy: '#ffb800',
+        darkgray: '#13131a',
+        darkgray50: '#13131a80',
+        main: '#21e5a9',
+        red: '#f33434',
+        sub01: '#4a4a4a',
+        sub02: '#6c6c6c',
+        sub03: '#d9d9d9',
+        white: '#ffffff',
+        white10: '#ffffff1a',
+        white70: '#ffffffb2',
+      }
+    },
   },
   plugins: [],
 };

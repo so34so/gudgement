408f9011fa068a21316efe33f4372c6607052ab0
kanghaeven <meerhae@gmail.com>
2023-09-07 17:57:01 +0900

    🚀Feat: 카카오 로그인 웹뷰 띄우기
    - redirect uri로 받아야 함

--- FE/Gudgement/src/pages/Login.tsx
+++ FE/Gudgement/src/pages/Login.tsx
@@ -1,9 +1,45 @@
+import { useState } from 'react';
 import { Image, View, Text, TouchableOpacity } from 'react-native';
+import { WebView } from 'react-native-webview';
+import { KAKAO_LOGIN_REST_API_KEY, KAKAO_LOGIN_REDIRECT_URI } from '@env';
+import Reactotron from 'reactotron-react-native';
+
+// 상단에 적어 탈취하여 웹뷰에 값을 가져오기
+const INJECTED_JAVASCRIPT = `window.ReactNativeWebView.postMessage('message from webView')`;
 
 function Login() {
+  
   const KakaoLogoImg = require('../assets/images/kakaologo.png')
+  
+  const [showWebView, setShowWebView] = useState(false);
+
   const handleLogin = () => {
-    console.log('로그인 클릭!');
+    setShowWebView(true);
+  }  
+
+  const getCode = (target: string) => {
+    const exp = 'code=';
+    const condition = target.indexOf(exp);
+    Reactotron.log!(target);
+  }
+
+  if (showWebView) {
+    return (
+      <WebView className='flex' 
+        // 웹뷰 보여줄 페이지 주소
+        source={{
+          uri: `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_LOGIN_REST_API_KEY}&redirect_uri=${KAKAO_LOGIN_REDIRECT_URI}&response_type=code`,
+        }}
+        // 웹뷰로 열리는 모든 페이지에서 실행될 자바스크립트 코드
+        injectedJavaScript={INJECTED_JAVASCRIPT}
+        javaScriptEnabled
+        // 웹뷰에서 window.ReactNativeWebView.postMessage 함수가 호출될때 실행되는 이벤트 함수
+        onMessage={event => {
+          const data = event.nativeEvent.url;
+          getCode(data);
+        }} 
+      />
+    );
   }
 
   return (

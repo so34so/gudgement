d4c03851163aab7380b11e88c9575e6d2ee42c7e
yms1789 <yms1789@naver.com>
2023-09-08 16:54:49 +0900

    🚑Fix: 버그 수정
    - 바텀탭 네비게이터, ShopNavigator lint 에러 수정

--- FE/Gudgement/src/navigation/BottomTabNavigator.tsx
+++ FE/Gudgement/src/navigation/BottomTabNavigator.tsx
@@ -1,50 +1,65 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from '../pages/Home';
-import MyPage from '../pages/MyPage';
-import Play from '../pages/Play';
-import Ranking from '../pages/Ranking';
-import { CommonType } from '../types/CommonType';
-import ShopNavigator from './ShopNavigator';
-import { Image, Text, View } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { BOTTOM_TAB_MENU } from '../utils/common';
-import TabBarItem from '../components/TabBarItem'
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "../pages/Home";
+import MyPage from "../pages/MyPage";
+import Play from "../pages/Play";
+import Ranking from "../pages/Ranking";
+import { CommonType } from "../types/CommonType";
+import ShopNavigator from "./ShopNavigator";
+import { Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { BOTTOM_TAB_MENU } from "../utils/common";
+import TabBarItem from "../components/TabBarItem";
 
 function BottomTabNavigator() {
   const Tab = createBottomTabNavigator<CommonType.RootStackParamList>();
-  const BottomTabComponents = [Home, ShopNavigator, Play, MyPage, Ranking]
+  const BottomTabComponents = [Home, ShopNavigator, Play, MyPage, Ranking];
   return (
-    <Tab.Navigator initialRouteName="홈" screenOptions={{
-      tabBarStyle: {
-        backgroundColor: "#4a4a4a"
-        , elevation: 0, borderTopWidth: 0, marginBottom: 80
-      }
-      , tabBarItemStyle: {
-        backgroundColor: '#4a4a4a',
-        height: 110,
-      },
-      tabBarLabel: (props) => {
-        return (
-          <TouchableOpacity className='w-16 h-8 rounded-md bg-black flex justify-center items-center'>
-            <View className='w-[95%] h-[92%] border-[1px] border-white rounded-md'>
-              <Text className='top-[2px] text-center text-white rounded-sm font-bold text-[16px]'>{props.children}</Text>
-            </View>
-          </TouchableOpacity >
-        )
-      }
-    }}>
+    <Tab.Navigator
+      initialRouteName="홈"
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: "#4a4a4a",
+          elevation: 0,
+          borderTopWidth: 0,
+          marginBottom: 80,
+        },
+        tabBarItemStyle: {
+          backgroundColor: "#4a4a4a",
+          height: 110,
+        },
+        // eslint-disable-next-line react/no-unstable-nested-components
+        tabBarLabel: props => {
+          return (
+            <TouchableOpacity className="w-16 h-8 rounded-md bg-black flex justify-center items-center">
+              <View className="w-[95%] h-[92%] border-[1px] border-white rounded-md">
+                <Text className="top-[2px] text-center text-white rounded-sm font-bold text-[16px]">
+                  {props.children}
+                </Text>
+              </View>
+            </TouchableOpacity>
+          );
+        },
+      }}
+    >
       {BOTTOM_TAB_MENU.map((ele, idx) => {
-        return (<Tab.Screen key={ele} name={ele as keyof CommonType.RootStackParamList} component={BottomTabComponents[idx]} options={{
-          headerShown: false,
-          tabBarIcon: (props) => {
-            return (<TabBarItem item={ele} />)
-          }
-        }} />);
+        return (
+          <Tab.Screen
+            key={ele}
+            name={ele as keyof CommonType.RootStackParamList}
+            component={BottomTabComponents[idx]}
+            options={{
+              headerShown: false,
+              // eslint-disable-next-line react/no-unstable-nested-components
+              tabBarIcon: () => {
+                return <TabBarItem item={ele} />;
+              },
+            }}
+          />
+        );
       })}
-
-    </Tab.Navigator >
-  )
+    </Tab.Navigator>
+  );
 }
 
 export default BottomTabNavigator;
--- FE/Gudgement/src/navigation/ShopNavigator.tsx
+++ FE/Gudgement/src/navigation/ShopNavigator.tsx
@@ -1,21 +1,22 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Shop from '../pages/Shop';
-import Inventory from '../pages/Inventory';
-import { CommonType } from '../types/CommonType';
-export default function ShopNavigator() {
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Shop from "../pages/Shop";
+import Inventory from "../pages/Inventory";
+import { CommonType } from "../types/CommonType";
+
+function ShopNavigator() {
   const Stack = createNativeStackNavigator<CommonType.RootStackParamList>();
   return (
-    <Stack.Navigator initialRouteName='Shop' screenOptions={{ headerShown: false }}>
-      <Stack.Screen
-        name="Shop"
-        component={Shop}
-        options={{ title: 'Shop' }}
-      />
+    <Stack.Navigator
+      initialRouteName="Shop"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="Shop" component={Shop} options={{ title: "Shop" }} />
       <Stack.Screen
         name="Inventory"
         component={Inventory}
-        options={{ title: 'Inventory' }}
+        options={{ title: "Inventory" }}
       />
     </Stack.Navigator>
-  )
+  );
 }
+export default ShopNavigator;

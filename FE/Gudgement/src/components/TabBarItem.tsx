0f58ba2ef0c46293fff86c12f93b4c91f976d6fe
yms1789 <yms1789@naver.com>
2023-09-08 17:12:08 +0900

    🚑Fix: 바텀 탭 네비게이션 이미지 타입 에러 수정

--- FE/Gudgement/src/components/TabBarItem.tsx
+++ FE/Gudgement/src/components/TabBarItem.tsx
@@ -1,4 +1,4 @@
-import { View, Image } from "react-native";
+import { View, Image, ImageSourcePropType } from "react-native";
 import Jilta from "../assets/images/jilta.png";
 
 interface ITabBarProps {
@@ -6,14 +6,18 @@ interface ITabBarProps {
 }
 
 function TabBarItem({ item }: ITabBarProps) {
+  const tabBarIcon: ImageSourcePropType = Jilta as ImageSourcePropType;
   return item === "플레이" ? (
     <View className="border-2 border-black top-3 w-[100%] h-[100%] flex justify-center items-center rounded-[999px] bg-white">
-      <Image source={Jilta} className="absolute h-[80%] w-[80%] top-4" />
+      <Image source={tabBarIcon} className="absolute h-[80%] w-[80%] top-4" />
     </View>
   ) : (
     <View className="border-2 border-black top-5 w-[80%] h-[80%] flex justify-center items-center rounded-[999px] bg-gray-400">
       <View className="w-[90%] h-[90%] rounded-[999px] bg-gray-900">
-        <Image source={Jilta} className="absolute h-full w-full bottom-4" />
+        <Image
+          source={tabBarIcon}
+          className="absolute h-full w-full bottom-4"
+        />
       </View>
     </View>
   );

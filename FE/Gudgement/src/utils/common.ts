67164970bedb8ff92c3533a3f464b4393327dd66
yms1789 <yms1789@naver.com>
2023-09-18 16:17:42 +0900

    🚀Feat: Text shadow 스타일 추가

--- FE/Gudgement/src/utils/common.ts
+++ FE/Gudgement/src/utils/common.ts
@@ -1,1 +1,6 @@
 export const BOTTOM_TAB_MENU = ["홈", "상점", "플레이", "내 정보", "랭킹"];
+export const textShadow = {
+  textShadowColor: "rgba(0, 0, 0, 0.5)", // 그림자의 색상과 투명도
+  textShadowOffset: { width: 2, height: 2 }, // 그림자의 위치 조정
+  textShadowRadius: 5, // 그림자의 블러 정도
+};

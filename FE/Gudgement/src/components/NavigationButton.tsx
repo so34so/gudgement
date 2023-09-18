f623d4014e90ef7431323a2e5889bbd60f75946e
kanghaeven <meerhae@gmail.com>
2023-09-18 17:54:52 +0900

    🚀Feat: NavigationButton 컴포넌트 커스텀

--- FE/Gudgement/src/components/NavigationButton.tsx
+++ FE/Gudgement/src/components/NavigationButton.tsx
@@ -1,59 +1,59 @@
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 import { CommonType } from "../types/CommonType";
 import { Text, Pressable, View } from "react-native";
-import Svg, { LinearGradient, Stop, Defs, Ellipse } from "react-native-svg";
 
 function NavigationButton({
   screenName,
   text,
+  height,
+  width,
+  size,
+  color,
 }: {
   screenName: keyof CommonType.RootStackParamList;
   text: string;
+  height: string;
+  width: string;
+  size: string;
+  color: string;
 }) {
   const navigation =
     useNavigation<NavigationProp<CommonType.RootStackParamList>>();
 
+  const buttonStyle01 = (color: string) =>
+    `h-fill w-fill py-[1px] flex flex-row justify-center items-center ${
+      color === "lightsky" ? "bg-lightsky" : "bg-deepgreen"
+    } border-solid border-[3px] border-white70 rounded-xl`;
+
+  const buttonStyle02 = (height: string, width: string) =>
+    `${height === "lg" ? "py-2" : "py-1"} ${
+      width === "lg" ? "px-[162px]" : "px-2"
+    } h-fill w-fill flex flex-row justify-center items-center bg-transparent border-solid border-[3px] border-white20 rounded-lg`;
+
+  const buttonStyle03 = (size: string) =>
+    `text-center text-white ${
+      size === "md" ? "text-md" : "text-sm"
+    } font-PretendardExtraBold`;
+
   return (
     <View>
-      <View>
-        <Svg height="150" width="300">
-          <Defs>
-            <LinearGradient id="grad" x1="50%" y1="50%" x2="50%" y2="0%">
-              <Stop offset="0" stopColor="#FFD080" stopOpacity="1" />
-              <Stop offset="1" stopColor="red" stopOpacity="1" />
-            </LinearGradient>
-          </Defs>
-        </Svg>
-        <Svg height="150" width="300">
-          <Ellipse cx="150" cy="75" rx="85" ry="55" fill="url(#grad)" />
-        </Svg>
-      </View>
       <Pressable
-        className="h-fill w-[160px] py-1 flex flex-row justify-center items-center bg-lightsky border-solid border-[3px] border-white70 rounded-lg"
+        className={buttonStyle01(color)}
         onPress={() => navigation.navigate(screenName)}
       >
-        <Svg height="100%" width="100%">
-          <Defs>
-            <LinearGradient id="grad" x1="0" y1="0" x2="100%" y2="0">
-              <Stop offset="0" stopColor="rgb(76,209,149)" />
-              <Stop offset="1" stopColor="#FF6F61" />
-            </LinearGradient>
-          </Defs>
-
-          <View className="h-fill w-[150px] py-1 flex flex-row justify-center items-center bg-transparent border-solid border-[3px] border-white20 rounded-lg">
-            <Text
-              style={{
-                textShadowColor: "rgba(0, 0, 0, 0.5)", // 그림자의 색상과 투명도
-                textShadowOffset: { width: 2, height: 2 }, // 그림자의 위치 조정
-                textShadowRadius: 5, // 그림자의 블러 정도
-              }}
-              className="text-center text-white text-md font-PretendardExtraBold"
-              numberOfLines={1}
-            >
-              {text}
-            </Text>
-          </View>
-        </Svg>
+        <View className={buttonStyle02(height, width)}>
+          <Text
+            style={{
+              textShadowColor: "rgba(0, 0, 0, 0.5)", // 그림자의 색상과 투명도
+              textShadowOffset: { width: 2, height: 2 }, // 그림자의 위치 조정
+              textShadowRadius: 5, // 그림자의 블러 정도
+            }}
+            className={buttonStyle03(size)}
+            numberOfLines={1}
+          >
+            {text}
+          </Text>
+        </View>
       </Pressable>
     </View>
   );

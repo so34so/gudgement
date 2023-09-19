41f1f3932e13d44c942fc198ebc306fb203f9ffa
kanghaeven <meerhae@gmail.com>
2023-09-19 17:36:05 +0900

    🚀Feat: 카카오 인가 코드 전달 백엔드 api 연결
    - 500 error

--- FE/Gudgement/src/pages/Login.tsx
+++ FE/Gudgement/src/pages/Login.tsx
@@ -10,7 +10,9 @@ import {
 } from "react-native";
 import { WebView, WebViewNavigation } from "react-native-webview";
 import { KAKAO_LOGIN_REST_API_KEY, KAKAO_LOGIN_REDIRECT_URI } from "@env";
-import KakaoLogoImg from "../assets/images/kakaologo.png";
+import KakaoLogoImg from "../assets/images/kakaoLogo.png";
+import axios from "axios";
+import Reactotron from "reactotron-react-native";
 
 // interface LoginProps {
 //   onLogin: () => void; // onLogin의 타입을 명시
@@ -27,17 +29,33 @@ function Login() {
     setShowWebView(true);
   };
 
-  const handleShouldStartLoad = (event: WebViewNavigation) => {
+  console.log(KAKAO_LOGIN_REDIRECT_URI);
+  Reactotron.log!(KAKAO_LOGIN_REDIRECT_URI);
+
+  const handleAuthorizationCode = (event: WebViewNavigation) => {
     const url = event.url;
-    console.log(url);
     // url에 붙어오는 code= 가있다면 뒤부터 parse하여 인가 코드 get
     const exp = "code=";
     const searchIdx = url.indexOf(exp);
     if (searchIdx !== -1) {
       const code = url.substring(searchIdx + exp.length);
       console.log("인가 코드", code);
+      try {
+        const response = axios.post(
+          `http://j9d106.p.ssafy.io:8080/oauth/kakao/callback?code=${code}`,
+        );
+        console.log("인가 코드 전달 성공!", response);
+        Reactotron.log!("인가 코드 전달 성공!", response);
+        // Authorization Code 전달하면
+        // Access Token 받기
+        // 받은 Access Token 다시 전달
+        // 유저 정보 받기
+        // 로그인 유지
+      } catch (error) {
+        Reactotron.log!("인가 코드 전달 실패!", error);
+      }
 
-      navigation.navigate("SettingLoggedIn");
+      navigation.navigate("SettingEmail");
 
       return false;
     }
@@ -52,11 +70,10 @@ function Login() {
         source={{
           uri: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_LOGIN_REST_API_KEY}&redirect_uri=${KAKAO_LOGIN_REDIRECT_URI}`,
         }}
-        onShouldStartLoadWithRequest={handleShouldStartLoad}
+        onShouldStartLoadWithRequest={handleAuthorizationCode}
       />
     );
   }
-
   return (
     <View className="flex flex-row justify-center items-end h-full pb-20">
       <Pressable

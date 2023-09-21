58e2f2b9034106208d39d924449edfae41d48496
yms1789 <yms1789@naver.com>
2023-09-21 10:54:45 +0900

    🚀Feat:  홈 화면 UI 및 기능 추가
    - 소비내역 퍼센테이지에 따라 절약 여부 출력
    - 소비내역에 따라 진행 바 게이지, 색상 변경되도록 구현

---
+++ FE/Gudgement/src/components/ProgressBar.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useRef, useState } from "react";
+import { View, StyleSheet, Animated } from "react-native";
+
+/**
+ * useEffect로 percent변경될 때 마다 애니메이션 동작하게끔 구현
+ * 애니메이션: 2초 동안 progress바 너비의 n%만큼의 검은색 바를 생성
+ * 애니메이션 완료되면 소비내역 퍼센테이지에 따라
+ * 절약일 때는 main색, 안정일 땐 무색, 위험일 때는 빨간 색으로 설정
+ */
+const ProgressBar = ({ percent }: { percent: number }) => {
+  const [progress] = useState(new Animated.Value(0));
+  const containerRef = useRef<View>(null);
+  const [progressColor, setProgressColor] = useState("bg-transparent");
+  useEffect(() => {
+    Animated.timing(progress, {
+      toValue: 370 * percent,
+      duration: 2000,
+      useNativeDriver: false,
+    }).start(() => {
+      if (percent <= 0.5) {
+        setProgressColor("bg-main");
+      }
+      if (percent > 0.5 && percent <= 0.7) {
+        setProgressColor("bg-transparent");
+      }
+      if (percent > 0.7) {
+        setProgressColor("bg-red");
+      }
+    });
+  }, [percent, progress]);
+
+  return (
+    <View
+      style={styles.container}
+      className={`border-2 border-white ${progressColor}`}
+      ref={containerRef}
+    >
+      <Animated.View
+        style={[styles.bar, { width: progress }]}
+        className="rounded-[10px]"
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: 370,
+    height: 24,
+    borderRadius: 10,
+    margin: 10,
+    elevation: 5,
+    zIndex: 1,
+  },
+  bar: {
+    height: 20,
+    backgroundColor: "#000000",
+  },
+});
+
+export default ProgressBar;

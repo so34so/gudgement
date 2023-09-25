bc0806fcd43230dc3e6c56a36e99aa711a133b77
jihun <hotchapa@naver.com>
2023-09-26 00:12:23 +0900

    🚀Feat: 플레이 큐 스크린 구현 (수락 거절)

---
+++ FE/Gudgement/src/pages/PlayGame.tsx
@@ -0,0 +1,24 @@
+import { useCallback, useEffect, useState } from "react";
+import FastImage, { FastImageProps } from "react-native-fast-image";
+import {
+  ImageBackground,
+  Image,
+  ImageSourcePropType,
+  Pressable,
+  Text,
+  View,
+} from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from "react-native-reanimated";
+import { View, Text } from "react-native";
+export default function PlayGame() {
+  return (
+    <View>
+      <Text>Multi</Text>
+    </View>
+  );
+}

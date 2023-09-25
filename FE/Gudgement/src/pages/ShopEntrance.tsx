2f444b610d7179a733b343231ad294e2d6a4d2d7
yms1789 <yms1789@naver.com>
2023-09-25 10:39:51 +0900

    🚀Feat: 상점 입장 아이콘 애니메이션 추가

--- FE/Gudgement/src/pages/ShopEntrance.tsx
+++ FE/Gudgement/src/pages/ShopEntrance.tsx
@@ -14,12 +14,38 @@ import Chijang from "../assets/icons/chijang.png";
 import Sobi from "../assets/icons/sobi.png";
 import Character from "../assets/icons/character.png";
 import { screenHeight, screenWidth } from "../utils/common";
-import reactotron from "reactotron-react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from "react-native-reanimated";
+import { useEffect } from "react";
 
 function ShopEntrance() {
   const navigation =
     useNavigation<NavigationProp<CommonType.RootStackParamList>>();
-  reactotron.log!(screenHeight);
+  const offset = useSharedValue(5);
+  const animatedRowStyles = useAnimatedStyle(() => ({
+    transform: [{ translateY: offset.value }],
+  }));
+  const animatedColStyles = useAnimatedStyle(() => ({
+    transform: [{ translateY: -offset.value }],
+  }));
+  useEffect(() => {
+    offset.value = withRepeat(
+      withTiming(-offset.value, { duration: 500 }),
+      -5,
+      true,
+    );
+  }, [offset]);
+  useEffect(() => {
+    offset.value = withRepeat(
+      withTiming(-offset.value, { duration: 500 }),
+      -5,
+      true,
+    );
+  }, [offset]);
   return (
     <View className="flex flex-1 w-full h-full">
       <ImageBackground
@@ -30,54 +56,69 @@ function ShopEntrance() {
         className="flex-1"
       >
         <View className="top-10">
-          <Pressable
-            onPress={() => navigation.navigate("Shop", { category: "소모품" })}
-          >
-            <Image
-              source={Sobi as ImageSourcePropType}
-              style={{
-                top: screenHeight / 2.6,
-                left: screenWidth / 2.5,
-              }}
-              className="absolute rounded-[20px] w-24 h-28"
-            />
-          </Pressable>
-          <Pressable
-            onPress={() => navigation.navigate("Shop", { category: "칭호" })}
-          >
-            <Image
-              source={Chingho as ImageSourcePropType}
-              style={{
-                top: screenHeight / 2,
-                left: screenWidth / 11,
-              }}
-              className="absolute rounded-[20px] w-24 h-28"
-            />
-          </Pressable>
-          <Pressable
-            onPress={() => navigation.navigate("Shop", { category: "치장" })}
-          >
-            <Image
-              source={Chijang as ImageSourcePropType}
-              style={{
-                top: screenHeight / 2,
-                left: screenWidth / 1.4,
-              }}
-              className="absolute rounded-[20px] w-24 h-28"
-            />
-          </Pressable>
-          <Pressable
-            onPress={() => navigation.navigate("Shop", { category: "캐릭터" })}
-          >
-            <Image
-              source={Character as ImageSourcePropType}
-              style={{
-                top: screenHeight / 1.6,
-                left: screenWidth / 2.5,
-              }}
-              className="absolute rounded-[20px] w-24 h-28"
-            />
-          </Pressable>
+          <Animated.View style={[animatedRowStyles]}>
+            <Pressable
+              onPress={() =>
+                navigation.navigate("Shop", { category: "소모품" })
+              }
+            >
+              <Image
+                source={Sobi as ImageSourcePropType}
+                style={{
+                  top: screenHeight / 2.6,
+                  left: screenWidth / 2.5,
+                }}
+                className="absolute rounded-[20px] w-24 h-28"
+              />
+            </Pressable>
+          </Animated.View>
+
+          <Animated.View style={[animatedColStyles]}>
+            <Pressable
+              onPress={() => navigation.navigate("Shop", { category: "칭호" })}
+            >
+              <Image
+                source={Chingho as ImageSourcePropType}
+                style={{
+                  top: screenHeight / 2,
+                  left: screenWidth / 11,
+                }}
+                className="absolute rounded-[20px] w-24 h-28"
+              />
+            </Pressable>
+          </Animated.View>
+
+          <Animated.View style={[animatedColStyles]}>
+            <Pressable
+              onPress={() => navigation.navigate("Shop", { category: "치장" })}
+            >
+              <Image
+                source={Chijang as ImageSourcePropType}
+                style={{
+                  top: screenHeight / 2,
+                  left: screenWidth / 1.4,
+                }}
+                className="absolute rounded-[20px] w-24 h-28"
+              />
+            </Pressable>
+          </Animated.View>
+
+          <Animated.View style={[animatedRowStyles]}>
+            <Pressable
+              onPress={() =>
+                navigation.navigate("Shop", { category: "캐릭터" })
+              }
+            >
+              <Image
+                source={Character as ImageSourcePropType}
+                style={{
+                  top: screenHeight / 1.6,
+                  left: screenWidth / 2.5,
+                }}
+                className="absolute rounded-[20px] w-24 h-28"
+              />
+            </Pressable>
+          </Animated.View>
         </View>
       </ImageBackground>
     </View>

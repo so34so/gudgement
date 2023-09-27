66b28aef3adfc0cc6eec3c70a35fe40fccb29db5
kanghaeven <meerhae@gmail.com>
2023-09-28 01:56:47 +0900

    💄Design: 만보기 차트, 보상받기 버튼 ui 구현

--- FE/Gudgement/src/pages/MyPage.tsx
+++ FE/Gudgement/src/pages/MyPage.tsx
@@ -1,4 +1,10 @@
-import { View, ImageBackground, Image } from "react-native";
+import {
+  View,
+  ImageBackground,
+  Image,
+  ColorValue,
+  StyleSheet,
+} from "react-native";
 
 import { ProgressChart } from "react-native-chart-kit";
 
@@ -14,7 +20,7 @@ import { useEffect } from "react";
 import { CommonType } from "../types/CommonType";
 import { IMAGE_URL } from "@env";
 
-function MyPage() {
+function MyPage(this: unknown) {
   const {
     data: userData,
     error: fetchError,
@@ -43,12 +49,13 @@ function MyPage() {
   }
 
   const data = {
-    // labels: ["pedometer", "Bike", "Run"], // optional
     data: [0.6],
-    // strokeWidth: 2,
-    // stroleColor: "#124518",
-    // borderColor: "#124518",
-    // borderWidth: 2,
+    colors: ["#3190FF"],
+  };
+
+  const dataSub = {
+    data: [0.6],
+    colors: ["#000000b2"],
   };
 
   const handleFetchMillion = async () => {
@@ -79,47 +86,54 @@ function MyPage() {
             />
             <BasicBox text={"뚜벅뚜벅뚜벅뚜벅..."} />
           </View>
-          <View>
-            <ProgressChart
-              data={data}
-              width={200}
-              height={220}
-              strokeWidth={30}
-              radius={59}
-              chartConfig={{
-                backgroundGradientFromOpacity: 0,
-                backgroundGradientToOpacity: 0,
-                // fillShadowGradientFrom: "#000000",
-                // fillShadowGradientFromOpacity: 1,
-                // fillShadowGradientFromOffset: 1,
-                decimalPlaces: 20, // optional, defaults to 2dp
-                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                labelColor: (opacity = 1) => `rgba(25, 25, 55, ${opacity})`,
-                style: {
-                  borderRadius: 16,
-                },
-                propsForDots: {
-                  r: "6",
-                  strokeWidth: "2",
-                  stroke: "#ffa726",
-                },
-              }}
-              style={{}}
-              hideLegend={true}
-            />
+          <View className="flex justify-center items-center mt-6">
+            <View className="flex justify-center items-center">
+              <ProgressChart
+                data={data}
+                width={200}
+                height={200}
+                strokeWidth={30}
+                radius={59}
+                chartConfig={{
+                  backgroundGradientFromOpacity: 0,
+                  backgroundGradientToOpacity: 0,
+                  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                }}
+                withCustomBarColorFromData={true}
+                style={{ zIndex: 1 }}
+                hideLegend={true}
+              />
+              <ProgressChart
+                data={dataSub}
+                width={200}
+                height={200}
+                strokeWidth={38}
+                radius={59}
+                chartConfig={{
+                  backgroundGradientFromOpacity: 0,
+                  backgroundGradientToOpacity: 0,
+                  color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+                }}
+                withCustomBarColorFromData={true}
+                style={{ position: "absolute", zIndex: 0 }}
+                hideLegend={true}
+              />
+            </View>
+            <View className="absolute z-20 pb-[150px]">
+              <NavigationButton
+                handleFunction={() => handleFetchMillion()}
+                text="보상 받기"
+                height="sm"
+                width="sm"
+                size="sm"
+                color="bluesky"
+              />
+            </View>
           </View>
         </View>
         <TagBoxSmall
           text={"이번달 소비 추이"}
           img={`${IMAGE_URL}/asset/analysisIcon.png`}
-        />
-        <NavigationButton
-          handleFunction={() => handleFetchMillion()}
-          text="만보 걷기"
-          height="sm"
-          width="sm"
-          size="sm"
-          color="lightsky"
         />
         <NavigationButton
           handleFunction={() => handleFetchMillion()}

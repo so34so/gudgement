a72027de13fd4a3d64bfadfb591c696ebb93f53d
권민우 <kwmw0427@naver.com>
2023-09-29 18:29:45 +0900

    Update ViewController.java

--- BE/Gudgenet/src/main/java/com/example/gudgement/member/controller/ViewController.java
+++ BE/Gudgenet/src/main/java/com/example/gudgement/member/controller/ViewController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ViewController {
-    @GetMapping("/oauth/kakao")
+    @GetMapping("/")
     public String redirect() {
 
         return "loading";

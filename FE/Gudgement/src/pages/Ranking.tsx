0fc1ba6971aae99788c63865c0c0c8967022dabd
yms1789 <yms1789@naver.com>
2023-10-04 16:23:39 +0900

    🚀Feat: 랭킹 정보 불러오는 로직 구현

--- FE/Gudgement/src/pages/Ranking.tsx
+++ FE/Gudgement/src/pages/Ranking.tsx
@@ -11,24 +11,29 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import Jilta from "../assets/images/jilta.png";
 import Svg, { Text as SvgText } from "react-native-svg";
-import { IMAGE_URL } from "@env";
 import { useQuery } from "@tanstack/react-query";
-import { CommonType } from "../types/CommonType";
+import Config from "react-native-config";
+import fetchApi from "../utils/tokenUtils";
+import { getAsyncData } from "../utils/common";
+import reactotron from "reactotron-react-native";
+import { AxiosResponse } from "axios";
 
 interface Irank {
-  rank: number;
-  name: string;
-  image: ImageSourcePropType;
-  level: number;
-}
-const RANKING = Array.from({ length: 10 }, (_, i) => {
-  return {
-    rank: i + 1,
-    name: `player${i}`,
-    image: Jilta as ImageSourcePropType,
-    level: 300 - i * 10,
+  rankingList: [
+    {
+      ranking: number;
+      nickname: string;
+      level: number;
+      character: string;
+    },
+  ];
+  myRanking: {
+    ranking: number;
+    nickname: string;
+    level: number;
+    character: string;
   };
-});
+}
 
 export default function Ranking() {
   const jilta: ImageSourcePropType = Jilta as ImageSourcePropType;
@@ -45,19 +50,42 @@ export default function Ranking() {
         return "bg-deepgreen";
     }
   };
-  console.log(IMAGE_URL);
-  const { data: user } = useQuery<CommonType.TUser>({
-    queryKey: ["fetchUserInfo"],
+  async function fetchRanking() {
+    const userId = await getAsyncData("id");
+    try {
+      const response: AxiosResponse<Irank> = await fetchApi.get(
+        `${Config.API_URL}/ranking`,
+        {
+          params: userId,
+        },
+      );
+      return response.data;
+    } catch (error) {
+      reactotron.log!(error);
+    }
+  }
+  const {
+    data: rankingList,
+    isSuccess,
+    isError,
+  } = useQuery({
+    queryKey: ["fetchRankingList"],
+    queryFn: () => fetchRanking(),
   });
-  const flatListProps: FlatListProps<Irank> = {
-    data: RANKING.slice(3, RANKING.length),
+  const flatListProps: FlatListProps<{
+    ranking: number;
+    nickname: string;
+    level: number;
+    character: string;
+  }> = {
+    data: rankingList?.rankingList?.slice(3, rankingList.rankingList.length),
     renderItem: ({ item }) => {
       return (
         <View className="w-full h-20 my-4 justify-center items-center">
           <View className="flex-row w-[90%] h-24 bg-white border-[3px] border-black rounded-[10px]">
             <View className="flex-row justify-start space-x-2 items-center ml-4 rounded-[10px]">
               <Text className="font-PretendardExtraBold text-[30px] text-black">
-                {item.rank}
+                {item.ranking}
               </Text>
               <Image source={jilta} />
               <Svg className="w-24 h-10">
@@ -70,12 +98,12 @@ export default function Ranking() {
                   y={30}
                   textAnchor="middle"
                 >
-                  {item.name}
+                  {item.nickname}
                 </SvgText>
               </Svg>
               <View
                 className={`relative ${styleLevel(
-                  item.rank,
+                  item.ranking,
                 )} flex flex-row justify-center items-center rounded-[20px] w-fit bg-white
       box-border h-8 text-center text-black border-[2px] border-solid border-black`}
               >
@@ -92,11 +120,14 @@ export default function Ranking() {
       );
     },
   };
+  if (isSuccess || isError) {
+    reactotron.log!("rankingList", rankingList);
+  }
   return (
     <SafeAreaView>
       <ImageBackground
         source={{
-          uri: `${IMAGE_URL}/asset/rankingBackground.png`,
+          uri: `${Config.IMAGE_URL}/asset/rankingBackground.png`,
         }}
         resizeMode="cover"
         style={{ opacity: 0.9, backgroundColor: "black" }}
@@ -104,31 +135,40 @@ export default function Ranking() {
       />
       <View className="w-full h-full bg-transparent">
         <View className="flex flex-row w-full justify-center space-x-10 mt-8 p-4 left-1">
-          {RANKING.slice(0, 3).map((ele: Irank) => (
-            <View key={ele.rank}>
-              <View
-                className={`${topRankColor(
-                  ele.rank,
-                )} border-2 border-black w-20 h-20 justify-center items-center rounded-[10px]`}
-              >
-                <View className="absolute top-[-17px] left-[-17px] flex justify-center items-center w-10 h-10 rounded-[99px] bg-white border-solid border-2 border-black">
-                  <View className="absolute w-9 h-9 bg-black border-2 flex justify-center items-center border-white rounded-[99px]">
-                    <Text className="text-white font-PretendardBold text-[22px]">
-                      {ele.rank}
-                    </Text>
+          {rankingList?.rankingList
+            ?.slice(0, 3)
+            .map(
+              (ele: {
+                ranking: number;
+                nickname: string;
+                level: number;
+                character: string;
+              }) => (
+                <View key={ele.ranking}>
+                  <View
+                    className={`${topRankColor(
+                      ele.ranking,
+                    )} border-2 border-black w-20 h-20 justify-center items-center rounded-[10px]`}
+                  >
+                    <View className="absolute top-[-17px] left-[-17px] flex justify-center items-center w-10 h-10 rounded-[99px] bg-white border-solid border-2 border-black">
+                      <View className="absolute w-9 h-9 bg-black border-2 flex justify-center items-center border-white rounded-[99px]">
+                        <Text className="text-white font-PretendardBold text-[22px]">
+                          {ele.ranking}
+                        </Text>
+                      </View>
+                    </View>
+                    <Image source={jilta} />
+                  </View>
+                  <View className="top-[-10px] left-[-7px] flex justify-center items-center w-24 h-fit rounded-[6px] bg-white border-solid border-2 border-black">
+                    <View className="w-full h-fit bg-black border-2 flex justify-center items-center border-white rounded-[6px]">
+                      <Text className="text-main font-PretendardBold text-[18px] px-2 py-1 whitespace-nowrap text-mainColor">
+                        {ele.nickname}
+                      </Text>
+                    </View>
                   </View>
                 </View>
-                <Image source={jilta} />
-              </View>
-              <View className="top-[-10px] left-[-7px] flex justify-center items-center w-24 h-fit rounded-[6px] bg-white border-solid border-2 border-black">
-                <View className="w-full h-fit bg-black border-2 flex justify-center items-center border-white rounded-[6px]">
-                  <Text className="text-main font-PretendardBold text-[18px] px-2 py-1 whitespace-nowrap text-mainColor">
-                    {ele.name}
-                  </Text>
-                </View>
-              </View>
-            </View>
-          ))}
+              ),
+            )}
         </View>
         <View className="h-[46%]">
           <FlatList {...flatListProps} />
@@ -137,7 +177,7 @@ export default function Ranking() {
           <View className="flex-row w-[90%] h-20 bg-mainColor border-[3px] border-black rounded-[10px]">
             <View className="flex-row justify-start space-x-2 items-center ml-4 rounded-[10px]">
               <Text className="font-PretendardExtraBold text-[30px] text-white">
-                22
+                {rankingList?.myRanking?.ranking}
               </Text>
               <Image source={jilta} />
               <Svg className="w-20 h-10">
@@ -150,7 +190,7 @@ export default function Ranking() {
                   y={30}
                   textAnchor="middle"
                 >
-                  {user?.nickname}
+                  {rankingList?.myRanking?.nickname}
                 </SvgText>
               </Svg>
               <View className="relative right-[-40px] flex flex-row justify-center items-center rounded-[20px] w-fit bg-white box-border h-8 text-center text-black border-[2px] border-solid border-black">
@@ -158,7 +198,7 @@ export default function Ranking() {
                   Lv
                 </Text>
                 <Text className="text-black text-[18px] font-PretendardBold px-2 w-12 text-center">
-                  {user?.level}
+                  {rankingList?.myRanking?.level}
                 </Text>
               </View>
             </View>

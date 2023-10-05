beb9f6cdd528e912376aa50d875b3cf38b677cc9
KwonMinWoo <kwmw0427@naver.com>
2023-10-05 17:49:47 +0900

    🚑Fix: 데이터 분석 Error 수정

--- BE/Gudgenet/src/main/java/com/example/gudgement/mypage/repository/ChartRepository.java
+++ BE/Gudgenet/src/main/java/com/example/gudgement/mypage/repository/ChartRepository.java
@@ -4,10 +4,12 @@ import com.example.gudgement.member.entity.Member;
 import com.example.gudgement.mypage.entity.Chart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ChartRepository extends JpaRepository<Chart, Long> {
     Optional<Chart> findByMemberIdAndYearAndMonthAndWeek(Member member,int year, int month, int week);
     void deleteAllByMemberId(Member member);
     Optional<Chart> findByAccountIdAndYearAndMonthAndWeek(Long accountId, int year, int month, int week);
+    List<Chart> findALLByAccountIdAndYearAndMonth(Long accountId, int year, int month);
 }

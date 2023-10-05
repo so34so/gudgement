25226ad64d60deb50d51dc21a8469fa238ba38e1
hotchapa <hotchapa@naver.com>
2023-10-05 16:27:04 +0900

    🚑Fix: Rendered fewer hooks than expected 에러 해결

--- FE/Gudgement/src/components/WebSocketContext.tsx
+++ FE/Gudgement/src/components/WebSocketContext.tsx
@@ -1,45 +1,10 @@
-// import React, { createContext, useContext, useEffect, useState } from "react";
-// import SockJS from "sockjs-client";
-// import { CompatClient, Stomp } from "@stomp/stompjs";
-
-// // 웹소켓 컨텍스트 생성
-// const WebSocketContext = createContext<CompatClient | null>(null);
-
-// export const WebSocketProvider: React.FC<{
-//   url: string;
-//   children: JSX.Element;
-// }> = ({ url, children }) => {
-//   const [client, setClient] = useState<CompatClient | null>(null);
-
-//   useEffect(() => {
-//     const newClient = Stomp.over(new SockJS(url));
-
-//     newClient.connect({}, frame => {
-//       console.log("Connected: " + frame);
-
-//       // useEffect 내에서 웹소켓 연결 후에 상태를 업데이트하여 context provider로 전달함
-//       setClient(newClient); // 연결 후 client 상태 업데이트
-//     });
-
-//     // Unmount 시점에 웹소켓 연결 종료
-//     return () => {
-//       if (newClient.connected) {
-//         newClient.disconnect();
-//       }
-//     };
-//   }, [url]);
-
-//   return (
-//     <WebSocketContext.Provider value={client}>
-//       {children}
-//     </WebSocketContext.Provider>
-//   );
-// };
-
-// // 커스텀 훅으로 컨텍스트 사용하기 쉽게 만들기
-// export const useWebSocket = () => useContext(WebSocketContext);
-
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import SockJS from "sockjs-client";
 import { CompatClient, Stomp } from "@stomp/stompjs";
 
@@ -51,30 +16,35 @@ export const WebSocketProvider: React.FC<{
   children: JSX.Element;
 }> = ({ url, children }) => {
   const [client, setClient] = useState<CompatClient | null>(null);
+  const connectAttemptedRef = useRef(false);
 
   useEffect(() => {
     const newClient = Stomp.over(new SockJS(url));
 
-    const connectAndSetupListeners = () => {
-      newClient.connect(
-        {},
-        frame => {
-          console.log("Connected: " + frame);
-          setClient(newClient);
-        },
-        error => {
-          // 에러 콜백 추가
-          console.log("WebSocket connection closed unexpectedly:", error);
-          setTimeout(connectAndSetupListeners, 5000); // 일정 시간 후 재연결 시도
-        },
-      );
+    newClient.onConnect = frame => {
+      console.log("Connected: " + frame);
+      setClient(newClient); // 연결 후 client 상태 업데이트
+      connectAttemptedRef.current = false; // 성공적으로 연결되면 재연결 시도 플래그를 리셋함
     };
 
-    connectAndSetupListeners();
+    newClient.onStompError = error => {
+      console.log("STOMP error:", error);
+
+      if (!connectAttemptedRef.current) {
+        connectAttemptedRef.current = true; // 처음 실패한 경우에만 재연결 시도 플래그를 설정함
+
+        setTimeout(() => {
+          console.log("Reconnecting...");
+          newClient.activate(); // 재연결 시도
+        }, 5000);
+      }
+    };
+
+    newClient.activate(); // 웹소켓 연결 활성화
 
     return () => {
       if (newClient.connected) {
-        newClient.disconnect();
+        newClient.deactivate(); // Unmount 시점에 웹소켓 연결 종료
       }
     };
   }, [url]);

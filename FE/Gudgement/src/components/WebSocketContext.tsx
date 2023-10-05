fe3c541c60b70d3c17c94369a167464620da2f98
hotchapa <hotchapa@naver.com>
2023-10-05 16:41:41 +0900

    🚑Fix: 일정시간 지날 시 웹소켓 연결 끊김 문제 해결

--- FE/Gudgement/src/components/WebSocketContext.tsx
+++ FE/Gudgement/src/components/WebSocketContext.tsx
@@ -1,9 +1,9 @@
 import React, {
   createContext,
   useContext,
+  useState,
   useEffect,
   useRef,
-  useState,
 } from "react";
 import SockJS from "sockjs-client";
 import { CompatClient, Stomp } from "@stomp/stompjs";
@@ -16,34 +16,39 @@ export const WebSocketProvider: React.FC<{
   children: JSX.Element;
 }> = ({ url, children }) => {
   const [client, setClient] = useState<CompatClient | null>(null);
-  const connectAttemptedRef = useRef(false);
+  const reconnectTimeoutIdRef = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
-    const newClient = Stomp.over(new SockJS(url));
+    let newClient: CompatClient;
 
-    newClient.onConnect = frame => {
-      console.log("Connected: " + frame);
-      setClient(newClient); // 연결 후 client 상태 업데이트
-      connectAttemptedRef.current = false; // 성공적으로 연결되면 재연결 시도 플래그를 리셋함
+    const connectAndSetupListeners = () => {
+      newClient = Stomp.over(new SockJS(url));
+
+      newClient.onConnect = frame => {
+        console.log("Connected: " + frame);
+        setClient(newClient); // 연결 후 client 상태 업데이트
+      };
+
+      newClient.onWebSocketClose = () => {
+        console.log("WebSocket closed.");
+        if (reconnectTimeoutIdRef.current === null) {
+          // 아직 재연결 시도 중이 아니라면 재연결을 시도합니다.
+          reconnectTimeoutIdRef.current = setTimeout(() => {
+            console.log("Reconnecting...");
+            connectAndSetupListeners();
+          }, 5000);
+        }
+      };
+
+      newClient.activate(); // 웹소켓 연결 활성화
     };
 
-    newClient.onStompError = error => {
-      console.log("STOMP error:", error);
-
-      if (!connectAttemptedRef.current) {
-        connectAttemptedRef.current = true; // 처음 실패한 경우에만 재연결 시도 플래그를 설정함
-
-        setTimeout(() => {
-          console.log("Reconnecting...");
-          newClient.activate(); // 재연결 시도
-        }, 5000);
-      }
-    };
-
-    newClient.activate(); // 웹소켓 연결 활성화
+    connectAndSetupListeners();
 
     return () => {
-      if (newClient.connected) {
+      if (newClient && newClient.connected) {
+        clearTimeout(reconnectTimeoutIdRef.current as NodeJS.Timeout); // 이미 예약된 재연결 타임아웃을 취소합니다.
+        reconnectTimeoutIdRef.current = null; // 재연결 타임아웃 ID를 리셋합니다.
         newClient.deactivate(); // Unmount 시점에 웹소켓 연결 종료
       }
     };

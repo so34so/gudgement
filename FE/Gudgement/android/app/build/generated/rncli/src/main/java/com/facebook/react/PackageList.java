2b2cb97e20d728ebd9d2badb271b867162ac0032
yms1789 <yms1789@naver.com>
2023-10-10 15:08:17 +0900

    Merge: 거지먼트 최종 코드 merge

---
+++ FE/Gudgement/android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -0,0 +1,124 @@
+
+package com.facebook.react;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.res.Resources;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainPackageConfig;
+import com.facebook.react.shell.MainReactPackage;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+// @react-native-async-storage/async-storage
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+// @react-native-firebase/analytics
+import io.invertase.firebase.analytics.ReactNativeFirebaseAnalyticsPackage;
+// @react-native-firebase/app
+import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
+// @react-native-firebase/messaging
+import io.invertase.firebase.messaging.ReactNativeFirebaseMessagingPackage;
+// appcenter
+import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
+// appcenter-analytics
+import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
+// appcenter-crashes
+import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
+// react-native-code-push
+import com.microsoft.codepush.react.CodePush;
+// react-native-fast-image
+import com.dylanvann.fastimage.FastImageViewPackage;
+// react-native-gesture-handler
+import com.swmansion.gesturehandler.RNGestureHandlerPackage;
+// react-native-permissions
+import com.zoontek.rnpermissions.RNPermissionsPackage;
+// react-native-push-notification
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+// react-native-reanimated
+import com.swmansion.reanimated.ReanimatedPackage;
+// react-native-safe-area-context
+import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
+// react-native-screens
+import com.swmansion.rnscreens.RNScreensPackage;
+// react-native-splash-screen
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+// react-native-webview
+import com.reactnativecommunity.webview.RNCWebViewPackage;
+// @react-native-community/slider
+import com.reactnativecommunity.slider.ReactSliderPackage;
+// react-native-config
+import com.lugg.RNCConfig.RNCConfigPackage;
+// react-native-health-connect
+import dev.matinzd.healthconnect.HealthConnectPackage;
+// react-native-svg
+import com.horcrux.svg.SvgPackage;
+
+public class PackageList {
+  private Application application;
+  private ReactNativeHost reactNativeHost;
+  private MainPackageConfig mConfig;
+
+  public PackageList(ReactNativeHost reactNativeHost) {
+    this(reactNativeHost, null);
+  }
+
+  public PackageList(Application application) {
+    this(application, null);
+  }
+
+  public PackageList(ReactNativeHost reactNativeHost, MainPackageConfig config) {
+    this.reactNativeHost = reactNativeHost;
+    mConfig = config;
+  }
+
+  public PackageList(Application application, MainPackageConfig config) {
+    this.reactNativeHost = null;
+    this.application = application;
+    mConfig = config;
+  }
+
+  private ReactNativeHost getReactNativeHost() {
+    return this.reactNativeHost;
+  }
+
+  private Resources getResources() {
+    return this.getApplication().getResources();
+  }
+
+  private Application getApplication() {
+    if (this.reactNativeHost == null) return this.application;
+    return this.reactNativeHost.getApplication();
+  }
+
+  private Context getApplicationContext() {
+    return this.getApplication().getApplicationContext();
+  }
+
+  public ArrayList<ReactPackage> getPackages() {
+    return new ArrayList<>(Arrays.<ReactPackage>asList(
+      new MainReactPackage(mConfig),
+      new AsyncStoragePackage(),
+      new ReactNativeFirebaseAnalyticsPackage(),
+      new ReactNativeFirebaseAppPackage(),
+      new ReactNativeFirebaseMessagingPackage(),
+      new AppCenterReactNativePackage(getApplication()),
+      new AppCenterReactNativeAnalyticsPackage(getApplication(), getResources().getString(com.dontspend.R.string.appCenterAnalytics_whenToEnableAnalytics)),
+      new AppCenterReactNativeCrashesPackage(getApplication(), getResources().getString(com.dontspend.R.string.appCenterCrashes_whenToSendCrashes)),
+      new CodePush(getResources().getString(com.dontspend.R.string.CodePushDeploymentKey), getApplicationContext(), com.dontspend.BuildConfig.DEBUG),
+      new FastImageViewPackage(),
+      new RNGestureHandlerPackage(),
+      new RNPermissionsPackage(),
+      new ReactNativePushNotificationPackage(),
+      new ReanimatedPackage(),
+      new SafeAreaContextPackage(),
+      new RNScreensPackage(),
+      new SplashScreenReactPackage(),
+      new RNCWebViewPackage(),
+      new ReactSliderPackage(),
+      new RNCConfigPackage(),
+      new HealthConnectPackage(),
+      new SvgPackage()
+    ));
+  }
+}
---
+++ FE/Gudgement/android/app/build/generated/source/buildConfig/release/com/dontspend/BuildConfig.java
@@ -0,0 +1,28 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dontspend;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.dontspend";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "1.1";
+  // Field from default config.
+  public static final String API_URL = "http://j9d106.p.ssafy.io:8080/api";
+  // Field from default config.
+  public static final String IMAGE_URL = "https://d15h0ofj6la3lc.cloudfront.net";
+  // Field from default config.
+  public static final boolean IS_HERMES_ENABLED = true;
+  // Field from default config.
+  public static final boolean IS_NEW_ARCHITECTURE_ENABLED = false;
+  // Field from default config.
+  public static final String KAKAO_LOGIN_REDIRECT_URI = "http://j9d106.p.ssafy.io:8080/";
+  // Field from default config.
+  public static final String KAKAO_LOGIN_REST_API_KEY = "4cd45261b839373b375e0766220b3428";
+  // Field from default config.
+  public static final String SERVER_URL = "http://j9d106.p.ssafy.io:8080/";
+  // Field from default config.
+  public static final String WEBSOCKET_URL = "http://j9d106.p.ssafy.io:8080/ws";
+}
